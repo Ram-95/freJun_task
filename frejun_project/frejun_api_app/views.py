354567f5a7e7ed0f54c1094263141612d651a252
Ram Babu <ram.kottapally@gmail.com>
2021-12-23 15:12:13 +0530

    Added redis and is working

--- frejun_project/frejun_api_app/views.py
+++ frejun_project/frejun_api_app/views.py
@@ -1,16 +1,20 @@
+import redis
+from django.core.cache import cache
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import PhoneNumber, Account
-from .authentication import AccountAuthentication
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from rest_framework.response import Response
 from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import AccountSerializer
 # Create your views here.
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
+                                  port=settings.REDIS_PORT, db=0)
 
 def index(request):
     return HttpResponse("Welcome to FreJun API.")
@@ -57,6 +61,15 @@ def inbound(request):
                 ph = PhoneNumber.objects.get(account=account_id, number=to)
             except PhoneNumber.DoesNotExist:
                 return Response({'message': '', 'errors': 'to parameter not found'})
+            
+            # Caching the from and to pair for 4 hours if text is STOP
+            if text.strip() == 'STOP':
+                if redis_instance.get(frm):
+                    print('FOUND IN CACHE.')
+                else:
+                    redis_instance.set(frm, to)
+                    #redis_instance.set("to", to)
+                    print("DATA STORED IN CACHE")
 
             return Response({'message': 'inbound sms ok', 'error': ''}, status=status.HTTP_200_OK)
         else:
@@ -64,8 +77,35 @@ def inbound(request):
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-
+@csrf_exempt
+@api_view(['POST'])
 def outbound(request):
-    return HttpResponse("You are at OUTBOUND SMS Route.")
+    if request.method == 'POST':
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            data = request.data
+            account = data.get('username')
+            frm = data.get('from', None)
+            to = data.get('to', None)
+            text = data.get('text', None)
+            input_params_list = [(frm,'from'), (to, 'to'), (text, 'text')] 
+            # Looping through the parameters
+            for k,v in input_params_list:
+                res_dict = data_validation(k, v)
+                if res_dict['errors'] != '':
+                    return Response(res_dict)
+
+            # Checking if "from" is present in PhoneNumber model for this account 
+            try:
+                account_id = Account.objects.get(username=account)
+                ph = PhoneNumber.objects.get(account=account_id, number=frm)
+            except PhoneNumber.DoesNotExist:
+                return Response({'message': '', 'errors': 'from parameter not found'})
+
+            return Response({'message': 'outbound sms ok', 'error': ''}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
 
 
--- frejun_project/frejun_project/settings.py
+++ frejun_project/frejun_project/settings.py
@@ -100,6 +100,20 @@ REST_FRAMEWORK = {
     ),
 }
 
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+CACHE_TTL = 60 * 15
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
